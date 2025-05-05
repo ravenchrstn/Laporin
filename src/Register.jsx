60942b95ecdd4ed0eafaf2deb58521f1b81dce1b
ravenchrstn <ravenchristian3@gmail.com>
2025-05-05 14:26:43 +0700

    fix merge conflict

--- src/Register.jsx
+++ src/Register.jsx
@@ -2,11 +2,5 @@ import { useState } from "react";
 
 function Menu() {
     const [menu, setMenu] = useState
-<<<<<<< Updated upstream
     // hehe
 }
-=======
-
-    // asd
-}
->>>>>>> Stashed changes
