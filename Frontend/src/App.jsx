d8b7965fce21d6b18b4a130c5deedc492f30bc8a
ravenchrstn <ravenchristian3@gmail.com>
2025-05-09 20:36:56 +0700

    inisialisasi kerja arshyil html css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import LoginPage from './pages/Login.jsx'
 import './tailwind.css'
 
 export default function App() {
   return (
     <>
-      
+      <LoginPage />
     </>
   )
 }
