3b82fe2229e1cbcd1d0111a3d9632d2b50183f9a
ravenchrstn <ravenchristian3@gmail.com>
2025-05-27 21:00:33 +0700

    bersih2-in folder dari file yang ga kepake

--- Frontend/src/components/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-    return (
-        <header className="header w-full h-15 flex justify-between px-header items-center">
-            <div className="text-xl text-white font-semibold">Laporin</div>
-            <img className="w-10 h-10 rounded-lg overflow-hidden" src="../../assets/anya.jpg" alt="profile picture"></img>
-        </header>
-    )
-}
